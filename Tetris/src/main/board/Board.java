2aaf6888d0c3763c6cb88bbab431c193ad8f469e
Kloong1 <dkdlsmsm98@gmail.com>
2022-04-28 16:20:57 +0900

    line clearing method

--- Tetris/src/main/board/Board.java
+++ Tetris/src/main/board/Board.java
@@ -1,6 +1,7 @@
 package board;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class Board implements Serializable {
 
@@ -11,5 +12,32 @@ public class Board implements Serializable {
 
     public boolean[][] getBoard() {
         return board;
+    }
+
+    public int clearLine() {
+        int clearedLines = 0;
+
+        for (int row = MAX_ROW; row >= 0; row--) {
+            if (isLineFilled(board[row])) {
+                clearedLines++;
+                Arrays.fill(board[row], false);
+            }
+            else if (clearedLines > 0) {
+                moveDownLine(row, clearedLines);
+            }
+        }
+
+        return clearedLines;
+    }
+
+    private void moveDownLine(int lineIdx, int clearedLines) {
+        System.arraycopy(board[lineIdx], 0, board[lineIdx + clearedLines], 0, MAX_COL);
+    }
+
+    private boolean isLineFilled(boolean[] line) {
+        for (boolean block : line)
+            if (!block)
+                return false;
+        return true;
     }
 }
